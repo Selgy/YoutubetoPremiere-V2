a316633619ee7aa3c69e622018980ca43da94b77
Selgy <imselgy@gmail.com>
2024-12-22 14:09:02 +0100

    v3.0.10

--- cep.config.ts
+++ cep.config.ts
@@ -23,7 +23,7 @@ const config: CEP_Config = {
   extensionManifestVersion: 6.0,
   requiredRuntimeVersion: 9.0,
   hosts: [
-    { name: "PPRO", version: "3.0.912" }
+    { name: "PPRO", version: "3.0.10" }
   ],
   type: "Panel",
   iconDarkNormal: "./src/assets/light-icon.png",
