ad8623612c8bc9333f5d43a6276d5fa1c99d66f0
Selgy <56513464+Selgy@users.noreply.github.com>
2025-06-10 11:28:32 +0200

    chore: bump version to 3.1.8
    - Updated package.json version
    - Updated Chrome extension manifest version
    - Updated project configuration files
    - Updated content script version references
    
    Release preparation for v3.1.8

--- project.config.js
+++ project.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // Configuration du projet
   project: {
     name: 'YoutubetoPremiere',
-    version: '3.1.7',
+    version: '3.1.8',
     description: 'Bridge entre YouTube et Adobe Premiere Pro'
   },
 
@@ -24,7 +24,7 @@ module.exports = {
   // Configuration CEP
   cep: {
     id: 'com.youtube.premiere',
-    version: '3.1.7',
+    version: '3.1.8',
     host: 'PPRO',
     minVersion: '13.0',
     maxVersion: '99.9'
