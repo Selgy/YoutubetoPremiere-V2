095f78883899787e6d453c1e75020f423d8ca9f1
Selgy <56513464+Selgy@users.noreply.github.com>
2025-06-10 13:08:23 +0200

    chore: bump version to 3.1.19
    - Updated package.json version
    - Updated Chrome extension manifest version
    - Updated project configuration files
    - Updated content script version references
    
    Release preparation for v3.1.19

--- project.config.js
+++ project.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // Configuration du projet
   project: {
     name: 'YoutubetoPremiere',
-    version: '3.1.18',
+    version: '3.1.19',
     description: 'Bridge entre YouTube et Adobe Premiere Pro'
   },
 
@@ -24,7 +24,7 @@ module.exports = {
   // Configuration CEP
   cep: {
     id: 'com.youtube.premiere',
-    version: '3.1.18',
+    version: '3.1.19',
     host: 'PPRO',
     minVersion: '13.0',
     maxVersion: '99.9'
