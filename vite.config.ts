636d409454c56cf55f861b04b8df9dd15133fbc3
Selgy <imselgy@gmail.com>
2025-07-31 10:57:17 +0200

    chore: bump version to 3.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -382,7 +382,8 @@ export default defineConfig({
 //   isPackage
 // );
 
-const currentVersion = process.env.APP_VERSION || '3.0.1';
+const currentVersion = process.env.APP_VERSION || '3.0.0';
+
 
 
 
