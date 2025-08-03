d8452f4d8ce2622303460a05db8593e9f416e991
Selgy <imselgy@gmail.com>
2025-08-03 12:13:56 +0200

    chore: bump version to 3.0.11

--- app/routes.py
+++ app/routes.py
@@ -84,14 +84,14 @@ def register_routes(app, socketio, settings):
 
     @app.route('/get-version', methods=['GET'])
     def get_version():
-        return jsonify(version='3.0.1')
+        return jsonify(version='3.0.11')
 
     @app.route('/check-updates', methods=['GET'])
     def check_updates():
         """Check for available updates from GitHub releases"""
         try:
             # Current version
-            current_version = '3.0.1'
+            current_version = '3.0.11'
             
             # Detect OS
             system = platform.system().lower()
@@ -868,6 +868,7 @@ def register_routes(app, socketio, settings):
 
 
     
+
 
 
 
